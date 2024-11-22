1c6d5fe1d1d85819b73c25f227efa862dff064a6
Ramón <ramon5130@hotmail.com>
2024-11-22 01:37:19 -0300

    Manejando commits

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 console.log("Hello world from main.js");
 console.log("Hola desde el repositorio remoto usando git pull");
 
-console.log("Haciendo una segunda prueba subiendo el archivo")
+console.log("Haciendo una segunda prueba subiendo el archivo");
+console.log("Pasos a seguir");
+console.log("1) git add .   o git main.js");
+console.log("2) git commit o git -M el mensaje entre comillas ");
+console.log("3) git push");
+console.log("4) para descargar desde el repositorio remoto usar git pull");
