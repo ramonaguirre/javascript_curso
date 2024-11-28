661d0d52eeb314aa36154a8b3520fc91c5013d32
Ramón <ramon5130@hotmail.com>
2024-11-28 20:20:38 -0300

    trabajando con js, curso de Fazt

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+var name = 'Roberto';
+let surname = 'Totototo';
+const PI = 3.1415;
+console.log(surname);
+
 console.log("Hello World"); // Muestra el mensaje por consola
 document.write("<h1>Hola Mundo</h1>"); //Muestra el mensaje por pantalla
-console.error("Mi error customizado");
+console.error("Mi error customizado"); //Muestra por consola un mensaje de error
+console.log('this is a string');
+console.log(true);
+console.log({
+   "username": 'Federico',
+   "score": 70.2,
+   "hours": 56,
+   "professional": true,
+   "friends": ["Victor", "Albino", "Rulo"] 
+})
+
+/*
+//Tipos de datos
+"Hola Mundo" //String
+'Hola MUndo' //String
+10000 //Number
+
+// boolean
+true
+false
+
+// array
+['Ramon', 'Gabriela', 'Kiki', 'Mia']
+[1,2,3,4]
+[true, false, true, false]
+
+// object
+
+      //"clave":valor,
+
+{
+    "username": 'Alfredo',
+    "score": 25.2,
+    "hours": 15,
+    "professional": true,
+    "friends": ['Samanta', 'Raúl', 'Marco', 'Andre']
+}
+
+{
+    "username": 'Jonathan',
+    "score": 5.3,
+    "hours": 2,
+    "professional": false,
+    "friends": ['Pedro', 'Micaela', 'Sergio']
+}
+*/
