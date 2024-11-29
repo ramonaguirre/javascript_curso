288d7460d35b0c37b2480d45c46653ef28e856f8
Ramón <ramon5130@hotmail.com>
2024-11-29 01:07:20 -0300

    Control de flujo, if, else if, else, switch, while...

--- main.js
+++ main.js
@@ -5,12 +5,6 @@ let n1 = 20;
 let n2 = 23;
 let result = n1 + n2;
 
-let passwordDB = 'loco1234';
-let input = 'loco1234';
-let check = input == passwordDB;
-console.log(check);
-//validación de contraseña
-
 console.log(surname);
 console.log("Hello World"); // Muestra el mensaje por consola
 document.write("<h1>Hola Mundo</h1>"); //Muestra el mensaje por pantalla
@@ -26,6 +20,53 @@ console.log({
 })
 console.log(result)
 console.log(name + " " + surname)
+
+//control de flujo
+let passwordDB = 'loco1234';
+let input = 'loco1234';
+let check = input == passwordDB;
+
+if (check == true){
+    console.log("Login Correcto");
+}
+else{
+    console.log("Login Incorrecto");
+}
+//validación de contraseña
+
+//seguimos con control de flujo
+let score = 70;
+
+if (score > 15){
+    console.log("Necesitas practicar más");
+}
+else if (score > 30){
+    console.log("Vas por el buen camino del programador Javascript");
+}
+else{
+    console.log("Vas por el mal camino")
+}
+
+// Switch
+let typeCard = 'DebitCard';
+
+switch(typeCard){
+    case 'DebitCard':
+        console.log("This is a debit card");
+        break;
+    case 'CreditCard':
+        console.log("This is a credit card");
+        break;
+    default:
+        console.log("You are have not card");
+}
+
+// Bucle
+let count = 1;
+while (count <= 10){
+    console.log("Hola Mundo" + " " + count);
+    count++;
+}
 
 /*
 //Tipos de datos
