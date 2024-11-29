4715f66079db8a634b5375b9766569889a888521
Ramón <ramon5130@hotmail.com>
2024-11-29 00:26:08 -0300

    variables, tipos de datos y estructura de datos

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var name = 'Chespirito';
 let surname = 'Totototo';
 const PI = 3.1415;
-console.log(surname);
+let n1 = 20;
+let n2 = 23;
+let result = n1 + n2;
 
+let passwordDB = 'loco1234';
+let input = 'loco1234';
+let check = input == passwordDB;
+console.log(check);
+//validación de contraseña
+
+console.log(surname);
 console.log("Hello World"); // Muestra el mensaje por consola
 document.write("<h1>Hola Mundo</h1>"); //Muestra el mensaje por pantalla
 console.error("Mi error customizado"); //Muestra por consola un mensaje de error
@@ -15,6 +24,8 @@ console.log({
    "professional": true,
    "friends": ["Victor", "Albino", "Rulo"] 
 })
+console.log(result)
+console.log(name + " " + surname)
 
 /*
 //Tipos de datos
